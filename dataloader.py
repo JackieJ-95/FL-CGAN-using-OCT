cec61ea8063e81425e15deb61dba7e57747e8fa2
JackieJ-95 <102839547+JackieJ-95@users.noreply.github.com>
2022-11-03 23:12:13 +0800

    Add files via upload

---
+++ dataloader.py
@@ -0,0 +1,21 @@
+import torch
+from torch.utils.data import DataLoader
+import torchvision
+import torchvision.transforms as transforms
+import numpy as np
+from PIL import Image
+from parameters import *
+
+def get_dataset():
+    transform = transforms.Compose([transforms.Resize((image_size, image_size)),
+#                                     transforms.CenterCrop(image_size),
+#                                    transforms.RandomCrop(32, padding=2),
+#                                    transforms.RandomHorizontalFlip(),                                    
+                                     transforms.ToTensor(),
+                                     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    dataset = torchvision.datasets.ImageFolder(root=dataroot,transform=transform)
+
+    return DataLoader(dataset, batch_size, shuffle=True, num_workers=workers, pin_memory=True)
+    
+    
---
+++ parameters.py
@@ -0,0 +1,39 @@
+# Root directory for dataset
+dataroot = "C:/Users/UIC/Desktop/science/GAN-Artwork-Generation-master/wikiart_resized/wikiart/"
+
+# Number of workers for dataloader
+workers = 8
+
+# Batch size during training
+batch_size = 128
+
+# Number of styles
+n_class = 27
+
+# Spatial size of training images. All images will be resized to this
+#   size using a transformer.
+image_size = 64
+
+# Number of channels in the training images. For color images this is 3
+nc = 3
+
+# Size of z latent vector (i.e. size of generator input)
+nz = 150
+
+# Size of feature maps in generator
+ngf = 64
+
+# Size of feature maps in discriminator
+ndf = 32
+
+# Number of training epochs
+num_epochs = 75
+
+# Learning rate for optimizers
+lr = 0.0001
+
+# Beta1 hyperparam for Adam optimizers
+beta1 = 0.5
+
+# Number of GPUs available. Use 0 for CPU mode.
+ngpu = 1
